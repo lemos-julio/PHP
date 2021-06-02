c7518deeb0921a15334b0277ecd036f6ac830bd8
lemos-julio <julio2012lemos@gmail.com>
2021-06-01 22:29:04 -0300

    01/06/2021

---
+++ telzir.php
@@ -0,0 +1,15 @@
+<?php
+  $OD = $_POST['OD'];
+  $FM = $_POST['FM'];
+  $OT = $_POST['OT'];
+  
+  
+  if($OD == '011-017' && $FM == '60'){
+      echo"Zero";
+  }else{
+      e;
+  }
+  
+  
+  
+?>
